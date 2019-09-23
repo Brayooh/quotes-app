a2a2a5fafe488b2f8b4e2909dcd524203011999f
brayooh <brianndichu.bn@gmail.com>
2019-09-23 11:24:11 +0300

    added class quotes.ts

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,8 +1,8 @@
-// export class Quoties {
-//     public showDescription: boolean;
-//     constructor (public id:number, public quote:string,public author:string, 
-//         public publisher:string, public upVotes:number,public downVote:number,public actualDate:Date){
-//         this.showDescription=false;
-//     }
+export class Quoties {
+    public showDescription: boolean;
+    constructor (public id:number, public quote:string,public author:string, 
+        public publisher:string, public upVotes:number,public downVote:number,public actualDate:Date){
+        this.showDescription=false;
+    }
     
-// }
+}
