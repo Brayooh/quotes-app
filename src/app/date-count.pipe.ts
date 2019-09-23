02233b2a4873001ca4b0c097955f2d1cdde664be
brayooh <brianndichu.bn@gmail.com>
2019-09-23 11:30:24 +0300

    edited files on pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,12 +1,11 @@
-// import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
-// @Pipe({
-//   name: 'dateCountPipe'
-// })
-// export class DateCountPipe implements PipeTransform {
+@Pipe({
+  name: 'dateCountPipe'
+})
+export class DateCountPipe implements PipeTransform {
+  transform(value: any, ...args: any[]): any {
+    return null;
+  }
 
-//   transform(value: any, ...args: any[]): any {
-//     return null;
-//   }
-
-// }
+}
